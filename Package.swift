0f3b37b47a39619772fcbba9524a6cbec13ce350
Yohannes Haile <yohannes@kemercode.com>
2024-09-16 13:37:02 +0300

    minimum support to iOS 14

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Chapa4iOS",
     
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
